23422fd859f03d78ce1a2b4bcc429915eba2e045
lautarohaili <lautarohaili@gmail.com>
2022-03-11 11:50:53 -0300

    Contador c/botones

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./Componentes/NavBar/NavBar";
 import ItemListContainer from "./Container/ItemListContainer";
+import ItemCount from "./Componentes/itemCount";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer saludo="Hola soy la nueva App" />
+      <ItemCount />
     </div>
   );
 }
